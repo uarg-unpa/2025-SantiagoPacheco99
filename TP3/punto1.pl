0dc951eedc696fa297142b4cac7f90cc269d9c41
SantiagoPacheco99 <161869143+SantiagoPacheco99@users.noreply.github.com>
2025-09-11 21:06:40 +0000

    Avance TP4

---
+++ TP3/punto1.pl
@@ -0,0 +1,13 @@
+:-dynamic(alumno/1).
+
+alumno(gonzales).
+alumno(perez).
+alumno(aguirre).
+escuela(gonzales, industrial6).
+escuela(perez, enet1).
+es_alumno(X,Y) :- alumno(X), escuela(X,Y).
+
+
+%assert(alumno(miranda)).
+
+%asserta(alumno(Herrera)).
